c34723ea531adac15c89a79594a64d65bb86fb7b
Carl Harroch <carl@novoda.com>
2013-09-01 23:24:38 +0100

    adding views

--- GPlus/src/main/java/com/cp/london/gplus/Landing.java
+++ GPlus/src/main/java/com/cp/london/gplus/Landing.java
@@ -1,23 +1,64 @@
 package com.cp.london.gplus;
 
+import android.annotation.TargetApi;
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.content.Loader;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
-public class Landing extends Activity {
+import java.util.Collections;
+import java.util.List;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class Landing extends Activity implements LoaderManager.LoaderCallbacks<List<String>> {
+
+    private ListView plusActivities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        plusActivities = (ListView) findViewById(R.id.listView);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.landing, menu);
         return true;
     }
-    
+
+    @Override
+    public Loader<List<String>> onCreateLoader(int i, Bundle bundle) {
+        return new ActivityLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<String>> listLoader, List<String> strings) {
+        plusActivities.setAdapter(new ActivityAdapter(
+                getApplicationContext(), R.layout.activity_item, strings
+        ));
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<String>> listLoader) {
+        plusActivities.setAdapter(null);
+    }
+
+    class ActivityLoader extends AsyncTaskLoader<List<String>> {
+
+        public ActivityLoader(Context context) {
+            super(context);
+        }
+
+        @Override
+        public List<String> loadInBackground() {
+            return Collections.singletonList("two");
+        }
+    }
+
 }
