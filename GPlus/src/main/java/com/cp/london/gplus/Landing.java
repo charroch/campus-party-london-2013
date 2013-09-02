23655a0e2b3a3c4b49b79b1d20d7cb14efc41b0b
Carl-Gustaf Harroch <carl@novoda.com>
2013-09-02 16:24:32 +0100

    adding initial fragm

--- GPlus/src/main/java/com/cp/london/gplus/Landing.java
+++ GPlus/src/main/java/com/cp/london/gplus/Landing.java
@@ -1,7 +1,9 @@
 package com.cp.london.gplus;
 
 import android.annotation.TargetApi;
+import android.app.Fragment;
 import android.app.ListActivity;
+import android.app.ListFragment;
 import android.app.LoaderManager;
 import android.content.AsyncTaskLoader;
 import android.content.Context;
@@ -9,26 +11,23 @@ import android.content.Loader;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
 import java.util.Collections;
 import java.util.List;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class Landing extends ListActivity implements LoaderManager.LoaderCallbacks<List<PlusActivity>> {
-
-    private ListView plusActivities;
-
-    private PlusAdapter adapter;
+public class Landing extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        plusActivities = (ListView) findViewById(android.R.id.list);
-        adapter = new PlusAdapter(this, Collections.<PlusActivity>emptyList());
     }
 
     @Override
@@ -45,33 +44,4 @@ public class Landing extends ListActivity implements LoaderManager.LoaderCallbac
                 return super.onOptionsItemSelected(item);
         }
     }
-
-    @Override
-    public Loader<List<PlusActivity>> onCreateLoader(int i, Bundle bundle) {
-        return new ActivityLoader(this);
-    }
-
-    @Override
-    public void onLoadFinished(Loader<List<PlusActivity>> listLoader, List<PlusActivity> activities) {
-        adapter = new PlusAdapter(getApplicationContext(), activities);
-        plusActivities.setAdapter(adapter);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<List<PlusActivity>> listLoader) {
-        plusActivities.setAdapter(null);
-    }
-
-    class ActivityLoader extends AsyncTaskLoader<List<PlusActivity>> {
-
-        public ActivityLoader(Context context) {
-            super(context);
-        }
-
-        @Override
-        public List<PlusActivity> loadInBackground() {
-            return Collections.<PlusActivity>emptyList();
-        }
-    }
-
 }
---
+++ GPlus/src/main/java/com/cp/london/gplus/MyPlusActivitiesFragment.java
@@ -0,0 +1,53 @@
+package com.cp.london.gplus;
+
+import android.annotation.TargetApi;
+import android.app.ListFragment;
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.content.Loader;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Collections;
+import java.util.List;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class MyPlusActivitiesFragment extends ListFragment implements LoaderManager.LoaderCallbacks<List<PlusActivity>> {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+
+    @Override
+    public Loader<List<PlusActivity>> onCreateLoader(int i, Bundle bundle) {
+        return new ActivityLoader(getActivity());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<PlusActivity>> listLoader, List<PlusActivity> activities) {
+        PlusAdapter adapter = new PlusAdapter(getActivity(), activities);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<PlusActivity>> listLoader) {
+        setListAdapter(null);
+    }
+
+    class ActivityLoader extends AsyncTaskLoader<List<PlusActivity>> {
+
+        public ActivityLoader(Context context) {
+            super(context);
+        }
+
+        @Override
+        public List<PlusActivity> loadInBackground() {
+            return Collections.<PlusActivity>emptyList();
+        }
+    }
+}
