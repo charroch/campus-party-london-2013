873500d3ed171facd341253c489a060bcd364775
Carl-Gustaf Harroch <carl@novoda.com>
2013-09-02 18:30:13 +0100

    adding initial dep + robotium test

---
+++ GPlus/src/instrumentTest/java/com/cp/london/gplus/test/RobotiumTest.java
@@ -0,0 +1,32 @@
+package com.cp.london.gplus.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.cp.london.gplus.Landing;
+import com.jayway.android.robotium.solo.Solo;
+
+import junit.framework.Assert;
+
+public class RobotiumTest extends ActivityInstrumentationTestCase2<Landing> {
+
+    private Solo solo;
+
+    public RobotiumTest() {
+        super(Landing.class);
+    }
+
+
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void test_should_show_menu_and_populate() throws Exception {
+        solo.sendKey(Solo.MENU);
+        Assert.assertTrue(solo.searchText("Populate"));
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+}
